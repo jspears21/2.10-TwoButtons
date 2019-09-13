c4c855686fd70a389252d6117ba6fc7f6be567a5
Jared Spears <jspears2@zagmail.gonzaga.edu>
2019-09-12 21:31:36 -0700

    hi

--- TwoButtons/ViewController.swift
+++ TwoButtons/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func button1(_ sender: UIButton) {
+        index = index + 1
+        label.text = String(index)
+    }
+    
+    
+    @IBAction func button2(_ sender: UIButton) {
+        index = index + 2
+        label.text = String(index)
+    }
+    
+   
+    
+    
+    
 }
 
